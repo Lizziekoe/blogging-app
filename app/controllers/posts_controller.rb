957c0c399b7a5b816551873706036327205b2101
Lizzie <lizziefkoehler@gmail.com>
2015-03-25 13:54:08 -0700

    add form for new blog post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,26 @@ class PostsController < ApplicationController
 		@posts = Post.all
 	end
 
+	def new
+		@post = Post.new
+	end
+
+	def create
+		@post = Post.new(params[:post])
+		if @post.save
+			redirect_to posts_path
+		else
+			render :new
+		end
+	end
+
 	def show
 		@post = Post.find(params[:id])
+	end
+
+	def update
+	end
+
+	def destroy
 	end
 end
